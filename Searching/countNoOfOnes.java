1577c2788b197b98f7931a2907a3e233f72633cd
sanskarsharma1822 <sanskarsharma1822@gmail.com>
2022-05-23 20:31:38 +0530

    Update countNoOfOnes

--- Searching/countNoOfOnes.java
+++ Searching/countNoOfOnes.java
@@ -43,8 +43,27 @@ public class countNoOfOnes {
         }
         return (last - first +1);
     }
+    static int better(int arr[]){
+        int start = 0 ; 
+        int end = arr.length - 1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(arr[mid]==1){
+                if(mid==0||arr[mid]!=arr[mid-1]){
+                    return arr.length - mid;
+                }else{
+                    end = mid-1;
+                }
+            }
+            else if(arr[mid]==0){
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
     public static void main(String args[]){
         int arr[] = {0,0,0,0,0,1,1,1,1,1,1};
         System.out.println("Count of 1s is : "+ans(arr,1));
+        System.out.println("Count of 1s is : "+better(arr));
     }
 }
