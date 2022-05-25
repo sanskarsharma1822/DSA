32f6c70aed388c204eb15d9e54c111b9010f5f78
sanskarsharma1822 <sanskarsharma1822@gmail.com>
2022-05-25 23:50:13 +0530

    Update findMedianOfTwoSortedArrays

--- Searching/findMedianOfTwoSortedArrays.java
+++ Searching/findMedianOfTwoSortedArrays.java
@@ -37,9 +37,42 @@ public class findMedianOfTwoSortedArrays {
         return combined.get((combined.size()-1)/2);
 
     }
+    static float better(int arr1[] , int arr2[]){
+        int start = 0;
+        int end = arr1.length;
+
+        while(start<=end){
+            int i1 = (start+end)/2;
+            int i2 = ((arr1.length+arr2.length+1)/2) - i1;
+            int left1 = i1==0?Integer.MIN_VALUE:arr1[i1-1];
+            int right1 = i1==arr1.length-1?Integer.MAX_VALUE:arr2[i1];
+            int left2 = i2==0?Integer.MIN_VALUE:arr2[i2-1];
+            int right2 = i2==arr1.length-1?Integer.MAX_VALUE:arr2[i2];
+
+            if(left1<=right2 && left2<=right1){
+                if((arr1.length + arr2.length)%2==0){
+                    System.out.println(Math.min(right1,right2)+ " "+Math.max(left1,left2));
+                    return ((float)Math.min(right1,right2) + (float)Math.max(left1,left2)) /2;
+                }
+                else{
+                    return (float)Math.max(left1,left2);
+                }
+            }
+            else{
+                if(left1>right2){
+                    end = i1-1;
+                }else{
+                    start = i1+1;
+                }
+            }
+
+        } 
+        return 0;
+    }
     public static void main(String args[]){
-        int arr1[] = {10,20,30,40,50,60};
-        int arr2[] = {5,15,25,35};
+        int arr2[] = {10,20,30,40,50,60};
+        int arr1[] = {5,15,25,35};
         System.out.println("Median of both arrays is : "+naive(arr1,arr2));
+        System.out.println("Median of both arrays is : "+better(arr1,arr2));
     }
 }
